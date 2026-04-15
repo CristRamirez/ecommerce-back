745d3af82954d67bd0a2f92f2f84f68512345c1d
Cris Ramirez <ramirezcris.cpr@gmail.com>
2026-04-15 10:18:56 -0300

    feat : primeros pasos en una api

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Hola")
+const express = require('express')
+const productos = require('./data/productos.json')
+const app = express()
+const PORT = "3005"
+
+app.get('/productos', ( _ , res) => {
+    res.status(200).json(productos)
+})
+
+app.get('/productos/:id', ( req , res) => {
+    const idProd = req.params.id
+
+    if(isNaN(idProd)) {
+        res.status(400).json({message: `Parametro de id incorrecto`})
+        return
+    }
+
+    const producto = productos.find(p => p.id === +idProd)
+    if (!producto) {
+        res.status(404).json({message:`El producto con id ${idProd} no se encuentra`})
+        return
+    }
+    res.status(200).json(producto)
+})
+
+app.post('/productos', (req, res) => {
+     
+})
+
+app.listen(PORT, (err) => {
+    if(err) {
+        console.error(err.message)
+        process.exit(1)
+    }
+    console.log(`La aplicacion esta escuchando en el puerto ${PORT}`) //interpolacion
+})
+
